28fd38b2498faa46098809f2c0567f4b89218b5e
Mr. DarkKnight <cacd_cacd@hotmail.com>
2025-11-19 15:57:17 +0300

    fix: remove .next from git tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"POVVbMMeX5oSIlsmUH+7EYefXs1mFWZ8w2Acr0qx6Vc=\"\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Eos2FVfpCeX/l+1PdqKdp0CH1MyuHbOLKu6Dz0j1nzY=\"\n}"
